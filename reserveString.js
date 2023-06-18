8c4f0a6ebd8c12b435dafdcf47ec2eb6a90a7011
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-18 17:03:57 +0300

    update

--- reserveString.js
+++ reserveString.js
@@ -1,3 +1,9 @@
 "use strict";
 
 let myString = prompt("enter your string here then okay: ");
+let myReservedStr;
+const reserveString = (str) => {
+  myReservedStr = str.split().reverse().join();
+};
+
+alert(myReservedStr);
